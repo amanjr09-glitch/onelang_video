b332d13a500d2410635f3177da93932fb8735bcf
Sasuke <atsyt09@gmail.com>
2023-05-19 19:53:02 +0530

    changes

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,19 +1,44 @@
+// import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { collection, getFirestore } from "firebase/firestore";
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: "react-auth-6788d.firebaseapp.com",
+//   projectId: "react-auth-6788d",
+//   storageBucket: "react-auth-6788d.appspot.com",
+//   messagingSenderId: "131797845021",
+//   appId: "1:131797845021:web:3f7ff4766e2b89ca5d32f4",
+//   measurementId: "G-VWPBR1NSLL",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const firebaseAuth = getAuth(app);
+// export const firebaseDB = getFirestore(app);
+// Import the functions you need from the SDKs you need
+
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { collection, getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "react-auth-6788d.firebaseapp.com",
-  projectId: "react-auth-6788d",
-  storageBucket: "react-auth-6788d.appspot.com",
-  messagingSenderId: "131797845021",
-  appId: "1:131797845021:web:3f7ff4766e2b89ca5d32f4",
-  measurementId: "G-VWPBR1NSLL",
+  apiKey: "AIzaSyDTIdHwgRxLgG2iJf2JIqWpF1vUe8_X8V4",
+  authDomain: "zoom-clone-951a3.firebaseapp.com",
+  projectId: "zoom-clone-951a3",
+  storageBucket: "zoom-clone-951a3.appspot.com",
+  messagingSenderId: "897082785829",
+  appId: "1:897082785829:web:ace71b55423a78b4267fcc",
+  measurementId: "G-NY8M54MTE7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const firebaseAuth = getAuth(app);
+const analytics = getAnalytics(app);
 export const firebaseDB = getFirestore(app);
+export const firebaseAuth = getAuth(app);
 
 export const usersRef = collection(firebaseDB, "users");
 export const meetingsRef = collection(firebaseDB, "meetings");
